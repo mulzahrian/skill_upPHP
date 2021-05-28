d605def3195edc0e1aba9c370486f115b7defc48
Mulzahrian_One <72987552+mulzahrian@users.noreply.github.com>
2021-05-28 10:16:29 +0700

    Update Narcissistic.php
    little modify

--- Narcissistic.php
+++ Narcissistic.php
@@ -1,8 +1,5 @@
 <?php
-// PHP program for checking of 
-// Narcissistic number 
- 
-// Function to count digits 
+
 function countDigit($n) 
 { 
     if ($n == 0) 
@@ -11,17 +8,12 @@ function countDigit($n)
     return (1 + countDigit($n / 10)); 
 } 
  
-// Returns true if n is
-// Narcissistic number 
 function check( $n) 
 { 
-    // count the number of digits 
     $l = countDigit($n); 
     $dup = $n; 
     $sum = 0; 
  
-    // calculates the sum of digits 
-    // raised to power 
     while ($dup) 
     { 
         $sum += pow($dup % 10, $l); 
@@ -31,14 +23,11 @@ function check( $n)
     return ($n == $sum); 
 } 
  
-// Driver Code 
 $n = 153; 
 echo("153 = ");
 if (check(!$n)) 
     echo "TRUE"; 
 else
     echo "FALSE"; 
- 
 
-// This code is contributed by akt_mit
 ?>
