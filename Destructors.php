fd07c9ae690f7fe49abd20dbd8e7cb0e6e2c041f
Mulzahrian_One <72987552+mulzahrian@users.noreply.github.com>
2021-06-08 08:59:20 +0700

    Update Destructors.php
    little modify

--- Destructors.php
+++ Destructors.php
@@ -1,12 +1,15 @@
 <?php
 class person {
+  //Object
   private $name;
   private $city;
+  //Cunstruct Function
   function __construct($name, $city) {
     $this->name = $name;
     $this->city = $city;
     echo $this->name." lives in ".$this->city.".\n";   
   }
+  //Destruct Function
   function __destruct() {
     echo "Destructor invoked for: ".$this->name."\n";
   }   
